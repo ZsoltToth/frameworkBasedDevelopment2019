fb60e0fab490c990ce7c4141933e3e14fef79763
Zsolt Toth <tothzs87@gmail.com>
2019-11-12 16:30:03 +0100

    ActorContoller is configured /actor/all end point.
    #5

--- sakila/src/main/java/hu/uni/eszterhazy/frameworks/ActorsController.java
+++ sakila/src/main/java/hu/uni/eszterhazy/frameworks/ActorsController.java
@@ -1,11 +1,20 @@
 package hu.uni.eszterhazy.frameworks;
 
+import hu.uni.eszterhazy.frameworks.model.Actor;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
+
 @RestController
+@RequestMapping(value = {"/actor"})
 public class ActorsController {
 
-    //public Collection<>
+    @RequestMapping(value={"/all"}, method = RequestMethod.GET)
+    public Collection<Actor> fetchAllActors(){
+        return null;
+    }
 }
