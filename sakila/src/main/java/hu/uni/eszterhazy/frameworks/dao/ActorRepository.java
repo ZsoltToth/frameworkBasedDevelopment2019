7d149880f1b5d11fd27cb452c10839211a88a6f0
Zsolt Toth <tothzs87@gmail.com>
2019-11-12 16:24:07 +0100

    ActorRepository is added.
    ActorRepository.java is the default implementation. It has no specific methods.
    
    #5

---
+++ sakila/src/main/java/hu/uni/eszterhazy/frameworks/dao/ActorRepository.java
@@ -0,0 +1,7 @@
+package hu.uni.eszterhazy.frameworks.dao;
+
+import hu.uni.eszterhazy.frameworks.Actor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor, Long> {
+}
