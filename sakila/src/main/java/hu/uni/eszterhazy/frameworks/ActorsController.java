bfac4fd80722148b331d8dc007553d13c026d503
Zsolt Toth <tothzs87@gmail.com>
2019-11-12 16:21:08 +0100

    ActonrController is added
    #5

---
+++ sakila/src/main/java/hu/uni/eszterhazy/frameworks/ActorsController.java
@@ -0,0 +1,11 @@
+package hu.uni.eszterhazy.frameworks;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+public class ActorsController {
+
+    //public Collection<>
+}
